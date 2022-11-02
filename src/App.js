c4b37600c75e83f862557d88b2d63a00d0d3ce6c
David Fragua <davidfragua@gmail.com>
2022-11-02 18:25:22 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
 
 const [ listFood, setListFood ] = useState(foods)
 
+const addFood = (oneFood) =>{
+    const copyListFood = [...listFood]
+    copyListFood.push(oneFood)
+    setListFood(copyListFood)
+}
 
 
   return (
     <div className="App">
+      <h2>Food List</h2>
       <Row style={{ width: '100%', justifyContent: 'center' }}>
       {listFood.map((eachFood, index) => {
         return (
@@ -29,6 +36,9 @@ const [ listFood, setListFood ] = useState(foods)
           }
           )}
           </Row>
+
+          <AddFoodForm  newFood={addFood}/>
+
           </div>
   );
 }
---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,61 @@
+import { Divider, Input } from 'antd';
+import { useState } from 'react';
+
+
+function AddFoodForm(props) {
+
+    const [ nameNew, setNameNew ] = useState("")
+    const [ imageNew, setImageNew ] = useState("")
+    const [ caloriesNew, setCaloriesNew ] = useState(0)
+    const [ servingsNew, setServingsNew ] = useState(0)
+
+    const addFood = (event) =>{
+
+                const foodToAdd ={
+                    name: nameNew,
+                    image: imageNew,
+                    calories: caloriesNew,
+                    servings: servingsNew
+                }
+           
+            props.newFood(foodToAdd)
+        }
+    
+    const handleChangeName = (event) => {
+        setNameNew(event.target.value)
+    }
+
+    const handleChangeImage = (event) => {
+        setImageNew(event.target.value)
+    }
+
+    const handleChangeCalories = (event) => {
+        setCaloriesNew(event.target.value)
+    }
+
+    const handleChangeServings = (event) => {
+        setServingsNew(event.target.value)
+    }
+
+  return (
+    <div>
+        <form>
+        <Divider>Add Food Entry</Divider>
+        <label htmlFor="name">Name</label>
+        <Input value={nameNew} type="text" name="name" onChange={handleChangeName} />
+        <label htmlFor="image">Image</label>
+        <Input value={imageNew} type="text" name="image" onChange={handleChangeImage} />
+        <label htmlFor="calories">Calories</label>
+        <Input value={caloriesNew} type="text" name="calories" onChange={handleChangeCalories} />
+        <label htmlFor="servings">Servings</label>
+        <Input value={servingsNew} type="text" name="servings" onChange={handleChangeServings} />
+        <button onClick={addFood}>Create</button>
+        </form>
+
+
+
+    </div>
+  )
+}
+
+export default AddFoodForm
